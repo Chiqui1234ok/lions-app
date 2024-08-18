6e34e26c69a9a578215d1cc0838b7ca4e8b4e98e
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-08-18 12:00:30 -0300

    I'm moving to a very object-oriented paradigm. So I'm deleting this solo files to fusion them into a class

--- back/src/helpers/validatePassword.ts
+++
@@ -1,7 +0,0 @@
-import IMongoUser from "../interfaces/IMongoUser";
-
-const validatePassword = async function(inputPassword: string, user: IMongoUser) {
-    return await user.validatePassword(inputPassword);
-}
-
-export default validatePassword;
