5dbaafc9483a7fd43789b7594164cae0b21640c8
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-08-18 12:01:37 -0300

    role is a mapping now, more convenient and flexible

--- back/src/models/User.ts
+++ back/src/models/User.ts
@@ -1,96 +1,30 @@
 import { Schema, model } from 'mongoose';
-import IMongoUser from '../interfaces/IMongoUser';
+import IMongoUser from '../interfaces/User';
 
-const UserSchema = new Schema(
+const UserSchema = new Schema<IMongoUser>(
     {
-        // name, description, contact photo, contact info and notes
-        name: {
-            type: String,
-            default: null
-        },
-        description: {
-            type: String,
-            required: false
-        },
-        phone: {
-            type: String,
-            required: false
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: false
-        },
-        businessID: [
-            {
-                type: Schema.ObjectId
-            }
-        ],
-        role: {
-            buy: [{
-                type: Number
-            }],
-            calendar: [{
-                type: Number
-            }],
-            calendarGoogle: [{
-                type: Number
-            }],
-            calendarMicrosoft: [{
-                type: Number
-            }],
-            clients: [{
-                type: Number
-            }],
-            emailing: [{
-                type: Number
-            }],
-            sale: [{
-                type: Number
-            }],
-            payment: [{
-                type: Number
-            }],
-            product: [{
-                type: Number
-            }],
-            shipping: [{
-                type: Number
-            }],
-            suppliers: [{
-                type: Number
-            }],
-            stripe: [{
-                type: Number
-            }],
-        },
-        thumbnail: {
-            type: Buffer,
-            required: false
-        },
-        note: {
-            type: String,
-            required: false
-        }
+        name: { type: String, default: null },
+        description: { type: String },
+        phone: { type: String },
+        email: { type: String },
+        password: { type: String },
+        role: { type: Map, of: Number },
+        thumbnail: { type: Buffer },
+        note: { type: String }
     },
     {
         timestamps: true
-    },
+    }
 );
 
-UserSchema.methods.encryptPassword = async (password: string) => {
-    // '20' is salt's value passes
+UserSchema.methods.encryptPassword = async function (password: string): Promise<string> {
     return Bun.password.hash(password);
 };
 
-UserSchema.methods.validatePassword = async function (password: string) {
-    // 'this.password' is hashed password on DB 
+UserSchema.methods.validatePassword = async function (password: string): Promise<boolean> {
     return Bun.password.verify(password, this.password);
 };
 
-// export type User = mongoose.InferSchemaType<typeof UserSchema>;
-// export const User = mongoose.model('User', UserSchema);
-export const User = model<IMongoUser>('User', UserSchema);
+const UserModel = model<IMongoUser>('User', UserSchema);
+
+export default UserModel;
