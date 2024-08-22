c70616aba57847a636a13a35b963965c80f5170e
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-08-22 19:44:52 -0300

    new "Validate" object

---
+++ back/src/classes/Validate.ts
@@ -0,0 +1,43 @@
+import { verify } from 'hono/jwt'
+
+/**
+ * This class encapsulates all validation's methods
+ * 
+ * @date 2024-08-22
+ * @author Santiago Gimenez
+ *
+ * @class Validate
+ * @typedef {Validate}
+ */
+class Validate {
+    /**
+     * Validates any string checking if it's an email
+     * 
+     * @param {string} email
+     * @returns {boolean} True if string contains '@' and '.<something>'.
+    */
+    email(email: string): boolean {
+        const regex = /^[^@]+@[^@]+\.[^@]+$/;
+        return regex.test(email);
+    }
+
+    /**
+     * Validates a JSON Web Token (JWT).
+     * 
+     * @param {string} token - The JSON Web Token to validate.
+     * @returns {boolean} True if the JWT is valid, otherwise false.
+     */
+    async jwt(token: string) {
+        const secret = process.env.DEV_JWT_SECRET;
+        let jwt = null;
+        if(token && secret) {
+            const splittedAuthorizationString = token.split('Bearer ');
+            jwt = await verify(splittedAuthorizationString[1], secret);
+            return jwt;
+        }
+        console.error(`jwt: ${jwt}`);
+        return false;
+    }
+}
+
+export default Validate;
