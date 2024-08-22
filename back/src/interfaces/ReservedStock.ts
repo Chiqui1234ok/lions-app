43d85c330b7c3e718f5ef69d287c4d5e78397376
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-08-21 22:03:16 -0300

    first commit

---
+++ back/src/interfaces/ReservedStock.ts
@@ -0,0 +1,7 @@
+// not implemented, yet
+interface ReservedStock {
+    productId: string;
+    totalQuantity: number;
+    averagePrice: number; // average price between all orders
+    orders: string[]; // IDs of boughts/orders
+}
