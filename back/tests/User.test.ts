fce2323219d4698b2b45d6fd59c0f17f6b4369ef
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-09-06 20:02:00 -0300

    pre-liminary support for Jest Test. Be patient...

---
+++ back/tests/User.test.ts
@@ -0,0 +1,20 @@
+import User from "../src/classes/User";
+import { Context } from "hono";
+import { mock } from 'jest-mock-extended'
+import { BaseUser } from "../src/interfaces/BaseUser";
+
+describe('User class testing', () => {
+    let instance: User;
+
+    beforeEach(() => {
+        instance = new User();
+    });
+
+    test('Cookie registering', () => {
+        const honoContext = mock<Context>(),
+                userData = mock<BaseUser>();
+        userData.mockReturnValue(instance);
+
+        expect(instance.registerUserCookies(honoContext, userData)).toBe(true);
+    })
+});
