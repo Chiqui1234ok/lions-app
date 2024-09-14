f6c10b7bc3acd9eded2a56ff3a9a450eb5efacd1
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-09-14 18:50:26 -0300

    'validate' function removed (I think isn't necessary, by now)

---
+++ back/src/classes/tinyTypes/Phone.ts
@@ -0,0 +1,25 @@
+class Phone {
+    constructor(
+        private country: string,
+        private number: string
+    ) {
+
+    }
+
+    // Getters
+    public get(): string {
+        return `${this.country} ${this.number}`;
+    }
+
+    // Setters
+    public set(country: string, number: string): string {
+        if(country === undefined || number === undefined)
+            throw new Error('Number and/or his country is undefined. Please, send us both values.');
+        
+        this.country = country;
+        this.number = number;
+        return `${country} ${number}`;
+    }
+
+    // Helpers
+}
