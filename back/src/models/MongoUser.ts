a0966d780e073406a4cc1bd07e001442669db524
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-09-25 18:45:27 -0300

    I centralized models, and MongoUser is the new implementation

---
+++ back/src/models/MongoUser.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+import User from '../classes/User/User';
+import Note from '../classes/tinyTypes/Note';
+import { Role } from '../classes/tinyTypes/Role';
+import { Name } from '../classes/tinyTypes/Name';
+import Phone from '../classes/tinyTypes/Phone';
+
+const schema = new Schema<User>(
+    {
+        name: { type: Name },
+        phone: { type: Phone },
+        email: { type: String },
+        password: { type: String },
+        thumbnail: { type: String },
+        userNotes: [ {type: Note} ],
+        admNotes: [ {type: Note} ],
+        roles: [ {type: Role} ],
+    },
+    {
+        timestamps: true
+    }
+);
+
+schema.methods.encryptPassword = async function (password: string): Promise<string> {
+    return Bun.password.hash(password);
+};
+
+schema.methods.validatePassword = async function (password: string): Promise<boolean> {
+    console.log('validatePassword(password):');
+    console.log(password);
+    return Bun.password.verify(password, this.password);
+};
+
+const MongoUser = model<User>('User', schema);
+
+export default MongoUser;
