ef2af2765ea4fbbe5ad05d366b1ff16dbf7342dc
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-09-26 17:36:36 -0300

    adjusted to Roles

--- back/src/classes/Client/Client.ts
+++ back/src/classes/Client/Client.ts
@@ -3,8 +3,9 @@ import Phone from '../tinyTypes/Phone';
 import Email from '../tinyTypes/Email';
 import Note from '../tinyTypes/Note';
 import Password from '../tinyTypes/Password';
-import { Role } from '../tinyTypes/Role';
+import { Roles } from '../tinyTypes/Roles';
 import Person from '../Person/Person';
+import Thumbnail from '../tinyTypes/Thumbnail';
 
 abstract class Client extends Person {
     constructor(
@@ -12,12 +13,42 @@ abstract class Client extends Person {
         protected phone: Phone,
         protected email: Email,
         protected password: Password,
-        protected thumbnail: string,
+        protected thumbnail: Thumbnail,
         protected userNotes: Note[],
         protected admNotes: Note[],
-        protected roles: Role[]
+        protected roles: Roles
     ) {
         super(name, phone, email, thumbnail, userNotes, admNotes);
+    }
+
+    // Getters
+    public getPassword = (): Password => this.password;
+    public getRoles = (): Roles => this.roles;
+
+    // Setters
+    public setName(names: string[]): void {
+        this.getName().set(names);
+    }
+    public addName(name: string): void {
+        this.getName().add(name);
+    }
+    public setAlias(alias: string): void {
+        this.getName().setAlias(alias);
+    }
+    public setEmail(email: string): void {
+        this.getEmail().set(email);
+    }
+    public setPassword(password: string): void {
+        this.getPassword().set(password);
+    }
+    public setThumbnail(thumbnailURL: string): void {
+        this.getThumbnail().set(thumbnailURL);
+    }
+    public setUserNotes(notes: Note[]): void {
+        this.getUserNotes().push(notes);
+    }
+    public setAdmNotes(notes: Note[]): void {
+        this.getAdmNotes().set(notes);
     }
 }
 
