7631b4362c6300fb613f88ac50adef33d59bb07b
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-09-26 12:06:48 -0300

    Now the implementation works with loadClass(User), not with and old mongoose schema (better for re-utilizing code in typescript)

--- back/src/models/MongoUser.ts
+++ back/src/models/MongoUser.ts
@@ -1,56 +1,11 @@
 // MongoDB
 import { Schema, model } from 'mongoose';
-import { prop, getModelForClass } from '@typegoose/typegoose'
 //
 import User from '../classes/User/User';
-import Note from '../classes/tinyTypes/Note';
-import { Role } from '../classes/tinyTypes/Role';
-import { Name } from '../classes/tinyTypes/Name';
-import Phone from '../classes/tinyTypes/Phone';
 
-const schema = new Schema<User>(
-    {
-        name: { 
-            type: Name
-        },
-        phone: { 
-            type: Phone
-        },
-        email: { 
-            type: String
-        },
-        password: { 
-            type: String
-        },
-        thumbnail: { 
-            type: String
-        },
-        userNotes: [{
-            type: Note
+const schema = new Schema();
+schema.loadClass(User);
 
-        }],
-        admNotes: [{
-            type: Note
-        }],
-        roles: [{
-            type: Role
-        }],
-    },
-    {
-        timestamps: true
-    }
-);
-
-schema.methods.encryptPassword = async function (password: string): Promise<string> {
-    return Bun.password.hash(password);
-};
-
-schema.methods.validatePassword = async function (password: string): Promise<boolean> {
-    console.log('validatePassword(password):');
-    console.log(password);
-    return Bun.password.verify(password, this.password);
-};
-
-const MongoUser = model<User>('User', schema);
+const MongoUser = model('User', schema);
 
 export default MongoUser;
