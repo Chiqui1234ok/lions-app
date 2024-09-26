0d4e5426da710a10034d144833643706330cfc6b
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-09-26 17:36:47 -0300

    added Thumbnail type

--- back/src/classes/Person/Person.ts
+++ back/src/classes/Person/Person.ts
@@ -2,13 +2,14 @@ import { Name } from '../tinyTypes/Name';
 import Phone from '../tinyTypes/Phone';
 import Email from '../tinyTypes/Email';
 import Note from '../tinyTypes/Note';
+import Thumbnail from '../tinyTypes/Thumbnail';
 
 abstract class Person {
     constructor(
         protected name: Name,
         protected phone: Phone,
         protected email: Email,
-        protected thumbnail: string,
+        protected thumbnail: Thumbnail,
         protected userNotes: Note[],
         protected admNotes: Note[]
     ) {
@@ -19,9 +20,26 @@ abstract class Person {
     public getName = (): Name => this.name;
     public getPhone = (): Phone => this.phone;
     public getEmail = (): Email => this.email;
-    public getThumbnail = (): string => this.thumbnail;
+    public getThumbnail = (): Thumbnail => this.thumbnail;
     public getUserNotes = (): Note[] => this.userNotes;
     public getAdmNotes = (): Note[] => this.admNotes;
+
+    // Setters
+    public setName(names: string | string[]): void {
+        if(Array.isArray(names))
+            this.getName().set(names);
+        else
+            this.getName().add(names);
+    }
+    public setAlias(alias: string): void {
+        this.getName().setAlias(alias);
+    }
+    public setEmail(email: string): void {
+        this.getEmail().set(email);
+    }
+    public setThumbnail(thumbnailURL: string): void {
+        this.getThumbnail().set(thumbnailURL);
+    }
 }
 
 export default Person;
